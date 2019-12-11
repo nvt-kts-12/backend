53f1e082c3e70873e9795e87fad33451731df3f0
markosimanic <marezver023@gmail.com>
2019-12-11 19:45:55 +0100

    Buy ticket and update event controller integration tests

--- src/test/java/nvt/kts/ticketapp/controller/event/EventControllerIntegrationTestsSuite.java
+++ src/test/java/nvt/kts/ticketapp/controller/event/EventControllerIntegrationTestsSuite.java
@@ -1,13 +1,15 @@
 package nvt.kts.ticketapp.controller.event;
 
 import nvt.kts.ticketapp.controller.event.searchAndFilter.SearchAndFilterIntegrationTest;
+import nvt.kts.ticketapp.controller.event.update.UpdateEventIntegrationTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         CreateEventIntegrationTest.class,
-        SearchAndFilterIntegrationTest.class
+        SearchAndFilterIntegrationTest.class,
+        UpdateEventIntegrationTest.class
 })
 public class EventControllerIntegrationTestsSuite {
 }
--- src/test/java/nvt/kts/ticketapp/controller/ticket/TicketControllerIntegrationTestsSuite.java
+++ src/test/java/nvt/kts/ticketapp/controller/ticket/TicketControllerIntegrationTestsSuite.java
@@ -1,5 +1,6 @@
 package nvt.kts.ticketapp.controller.ticket;
 
+import nvt.kts.ticketapp.controller.ticket.buy.BuyTicketIntegrationTest;
 import nvt.kts.ticketapp.controller.ticket.show.bought.ShowBoughtIntegrationTest;
 import nvt.kts.ticketapp.controller.ticket.show.reservations.ShowReservationsIntegrationTest;
 import org.junit.runner.RunWith;
@@ -8,7 +9,8 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         ShowReservationsIntegrationTest.class,
-        ShowBoughtIntegrationTest.class
+        ShowBoughtIntegrationTest.class,
+        BuyTicketIntegrationTest.class
 })
 public class TicketControllerIntegrationTestsSuite {
 }
