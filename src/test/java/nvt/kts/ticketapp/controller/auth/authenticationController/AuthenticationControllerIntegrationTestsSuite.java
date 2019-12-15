13fe631aa276898d580a4ad15b433ae70e6116fd
milosrasevic <mrasevic1@gmail.com>
2019-12-15 16:39:26 +0100

    Login tests

--- src/test/java/nvt/kts/ticketapp/controller/auth/authenticationController/AuthenticationControllerIntegrationTestsSuite.java
+++ src/test/java/nvt/kts/ticketapp/controller/auth/authenticationController/AuthenticationControllerIntegrationTestsSuite.java
@@ -1,6 +1,7 @@
 package nvt.kts.ticketapp.controller.auth.authenticationController;
 
 import nvt.kts.ticketapp.controller.auth.authenticationController.edit.EditUserIntegrationTest;
+import nvt.kts.ticketapp.controller.auth.authenticationController.login.LoginIntegrationTest;
 import nvt.kts.ticketapp.controller.auth.authenticationController.register.RegisterIntegrationTest;
 import nvt.kts.ticketapp.controller.auth.authenticationController.show.ShowUserIntegrationTest;
 import org.junit.runner.RunWith;
@@ -10,7 +11,8 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         RegisterIntegrationTest.class,
         EditUserIntegrationTest.class,
-        ShowUserIntegrationTest.class
+        ShowUserIntegrationTest.class,
+        LoginIntegrationTest.class
 })
 public class AuthenticationControllerIntegrationTestsSuite {
 }
