b5c087ecbb76a149b5d07f3a98902acb59c9304a
milosrasevic <mrasevic1@gmail.com>
2020-01-17 15:29:30 +0100

    CORS configured

---
+++ src/main/java/nvt/kts/ticketapp/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package nvt.kts.ticketapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import javax.servlet.*;
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("PUT", "GET", "POST", "DELETE");
+            }
+        };
+    }
+}
